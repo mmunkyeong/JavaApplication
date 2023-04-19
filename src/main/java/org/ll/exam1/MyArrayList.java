7dc80c524e18902b0d438dbdab06f6b7f96edb70
mmunkyeong <jmk4379@naver.com>
2023-04-19 14:43:01 +0900

    refactor

--- src/main/java/org/ll/exam1/MyArrayList.java
+++ src/main/java/org/ll/exam1/MyArrayList.java
@@ -1,12 +1,16 @@
-package org.ll;
+package org.ll.exam1;
 
 public class MyArrayList<T> {
     private int size=0;
     public int size(){
-        return 0;
+        return size;
     }
-    public boolean add(T element){
+    public boolean add(String element){
         size++;
         return true; // 항상 true를 리턴
+    }
+
+    public String get(int index) {
+        return "grape";
     }
 }
--- src/test/java/com/ll/exam/MyArrayListTest.java
+++ src/test/java/com/ll/exam/MyArrayListTest.java
@@ -2,7 +2,7 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.ll.MyArrayList;
+import org.ll.exam1.MyArrayList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -21,6 +21,15 @@ public class MyArrayListTest {
         MyArrayList<String> list=new MyArrayList<>();
         list.add("apple");
         list.add("grape");
-        assertThat(list.size()).isEqualTo(0);
+        assertThat(list.size()).isEqualTo(2);
+    }
+    @Test
+    @DisplayName("get(1)")
+    void t3() {
+        MyArrayList<String> list=new MyArrayList<>();
+        list.add("apple");
+        list.add("grape");
+        assertThat(list.get(1)).isEqualTo("grape");
+        // get은 이렇게 작동 되어야 한다. 전제하여 만들기
     }
 }
