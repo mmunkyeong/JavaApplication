272bdd9d2250494b2d87d33d87c8be5266418edd
mmunkyeong <jmk4379@naver.com>
2023-04-19 14:33:41 +0900

    list add

--- src/main/java/org/ll/MyArrayList.java
+++ src/main/java/org/ll/MyArrayList.java
@@ -1,7 +1,12 @@
 package org.ll;
 
 public class MyArrayList<T> {
+    private int size=0;
     public int size(){
         return 0;
+    }
+    public boolean add(T element){
+        size++;
+        return true; // 항상 true를 리턴
     }
 }
--- src/test/java/com/ll/exam/MyArrayListTest.java
+++ src/test/java/com/ll/exam/MyArrayListTest.java
@@ -1,14 +1,26 @@
 package com.ll.exam;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.ll.MyArrayList;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class MyArrayListTest {
+
     @Test
-    void testSize() {
+    @DisplayName("testSize")
+    void t1() {
         MyArrayList<String> list=new MyArrayList<>();
-        assertEquals(0,list.size());
+        assertThat(list.size()).isEqualTo(0);
+    }
+    @Test
+    @DisplayName("testAdd")
+    void t2() {
+        MyArrayList<String> list=new MyArrayList<>();
+        list.add("apple");
+        list.add("grape");
+        assertThat(list.size()).isEqualTo(0);
     }
 }
