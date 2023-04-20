259b21ef08649113449afc3f8c91080774f4196a
mmunkyeong <jmk4379@naver.com>
2023-04-20 11:28:29 +0900

    indexOf 개선

--- src/main/java/org/ll/exam1/MyArrayList.java
+++ src/main/java/org/ll/exam1/MyArrayList.java
@@ -3,66 +3,79 @@ package org.ll.exam1;
 import java.util.stream.IntStream;
 
 public class MyArrayList<T> {
-    public boolean debug=false;
+    public boolean debug = false;
     private Object[] data;
-    private int size=0;
+    private int size = 0;
 
-    public MyArrayList(){
+    public MyArrayList() {
         this(2);
     }
-    public MyArrayList(int dataLength){
-        data=new Object[dataLength];
+
+    public MyArrayList(int dataLength) {
+        data = new Object[dataLength];
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
-    public boolean add(T element){
+
+    public boolean add(T element) {
         // 만약에 공간이 부족하면 새 data 객체를 만든다.
         makeNewDataIfNotEnough();
-        data[size]=element;
+        data[size] = element;
         size++;
         return true; // 항상 true를 리턴
     }
 
-    private void makeNewDataIfNotEnough(){
+    private void makeNewDataIfNotEnough() {
         //먼저 공간이 부족한지 확인
-        if(ifNotEnought()){
+        if (ifNotEnought()) {
             makeNewData();
         }
     }
 
-    private void makeNewData(){
+    private void makeNewData() {
         // 새 배열을 만든다. (새 업체를 만든다)
-        Object[] newData=new Object[data.length*2];
+        Object[] newData = new Object[data.length * 2];
 
         // 기존 창고에 있단 물품들을 전부 새 창고로 옮긴다.
-        for(int i=0; i<data.length; i++){
-            newData[i]=data[i];
+        for (int i = 0; i < data.length; i++) {
+            newData[i] = data[i];
         }
 
-        if(debug){
+        if (debug) {
             System.out.printf("배열크기 증가 : %d => %d\n", data.length, newData.length);
         }
         // 기존 창고와 계약을 해지
         // 더 이상 리스트가 기존 배열을 가리키지 않도록 하여, 자연스럽게 가비지컬렉팅이 되도록 한다
-        data=newData;
+        data = newData;
     }
 
-    private boolean ifNotEnought(){
-        return size>=data.length;
+    private boolean ifNotEnought() {
+        return size >= data.length;
     }
 
     public T get(int index) {
-        return (T)data[index];
+        return (T) data[index];
     }
 
-    public int indexOf(T element){
+    public int indexOf(T element) {
+       /*
         for(int i=0; i<data.length; i++){
             if(element.equals(data[i])) return i;
         }
         return -1;
+        */
 
+        //더 깔끔한 버전
+        return IntStream.range(0, size)
+                .filter(index ->
+                        element.equals(data[index]))
+                .findFirst()
+                .orElse(-1);
+
+        // 복잡한 연산까지 할 수 있는 버전
+        // 예를 들어서 index와 element에 대한 복합조건을 filter에 사용가능
      /*   IntStream.range(0,size)
                 .mapToObj(index->new Object[] {index, data[index]})
                 .filter(arr->element.equals(arr[1]))
